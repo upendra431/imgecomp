29aef35899525b9167909ce4e062c1957c469704
manike upendra kumar <uppu431@gmail.com>
2020-09-10 10:32:08 +0530

    Update Index.java

--- src/main/java/Index.java
+++ src/main/java/Index.java
@@ -1,3 +1,4 @@
+package com.compress;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
